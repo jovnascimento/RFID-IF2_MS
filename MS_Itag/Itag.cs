27b287503d8b06f6339ed50be96eec200381c8af
jovnascimento <jovnascimento08@hotmail.com>
2022-12-03 15:58:48 -0300

    Tag e Log consumers

---
+++ MS_Itag/Itag.cs
@@ -0,0 +1,69 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Npgsql;
+
+class Receive
+{
+    //Npgsql
+    private static string connstring = String.Format("Server={0};Port={1};" +
+        "User Id={2};Password={3};Database={4};",
+        "localhost", 5432, "postgres",
+        "password", "RFID");
+    private static NpgsqlConnection? conn;
+
+    private static void Tag(string sql)
+    {
+        conn = new NpgsqlConnection(connstring);
+        conn.Open();
+
+        string dml = sql;
+
+        using (NpgsqlCommand cmd = new NpgsqlCommand(dml, conn))
+        {
+            try
+            {
+                cmd.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                conn.Close();
+                Console.WriteLine("Erro: " + e);
+            }
+        }
+    }
+
+    public static void Main()
+    {
+        var factory = new ConnectionFactory()
+        {
+            HostName = "localhost"
+        };
+        using (var connection = factory.CreateConnection())
+        using (var channel = connection.CreateModel())
+        {
+            channel.QueueDeclare(queue: "Itag",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+            var consumer = new EventingBasicConsumer(channel);
+
+            consumer.Received += (model, ea) =>
+            {
+                var body = ea.Body.ToArray();
+                var message = Encoding.UTF8.GetString(body);
+                Tag(message);
+                Console.WriteLine(" [x] Received {0}", message);
+            };
+            channel.BasicConsume(queue: "Itag",
+                                 autoAck: true,
+                                 consumer: consumer);
+
+            Console.WriteLine("Microserviço Itag");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ MS_RRlog/RRlog.cs
@@ -0,0 +1,104 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+using Npgsql;
+using Newtonsoft.Json;
+
+class Receive
+{
+    //Npgsql
+    private static string connstring = String.Format("Server={0};Port={1};" +
+        "User Id={2};Password={3};Database={4};",
+        "localhost", 5432, "postgres",
+        "password", "RFID");
+    private static NpgsqlConnection? conn;
+
+    private static NpgsqlDataReader Logs(string sql)
+    {
+        conn = new NpgsqlConnection(connstring);
+        conn.Open();
+
+        string dml = sql;
+        NpgsqlDataReader? reader = null;
+
+        using (NpgsqlCommand cmd = new NpgsqlCommand(dml, conn))
+        {
+            try
+            {
+                reader = cmd.ExecuteReader();
+            }
+            catch (Exception e)
+            {
+                conn.Close();
+                Console.WriteLine("Erro : " + e);
+            }
+        }
+        return reader;
+    }
+
+    //Serialização
+    public static IEnumerable<Dictionary<string, object>> Serialize(NpgsqlDataReader reader)
+    {
+        var results = new List<Dictionary<string, object>>();
+        var cols = new List<string>();
+        for (var i = 0; i < reader.FieldCount; i++)
+            cols.Add(reader.GetName(i));
+
+        while (reader.Read())
+            results.Add(SerializeRow(cols, reader));
+
+        return results;
+    }
+
+    private static Dictionary<string, object> SerializeRow(IEnumerable<string> cols,
+                                                NpgsqlDataReader reader)
+    {
+        var result = new Dictionary<string, object>();
+        foreach (var col in cols)
+            result.Add(col, reader[col]);
+        return result;
+    }
+
+    public static void Main()
+    {
+        NpgsqlDataReader? reader = null;
+
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+        channel.QueueDeclare(
+            "RRlog",
+            durable: false,
+            exclusive: false,
+            autoDelete: false,
+            arguments: null);
+
+        var consumer = new EventingBasicConsumer(channel);
+
+        consumer.Received += (model, ea) =>
+        {
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received request: {message}");
+
+            reader = Logs(message);
+            var r = Serialize(reader);
+            string json = JsonConvert.SerializeObject(r, Formatting.Indented);
+
+            var replyMessage = json;
+            var replyBody = Encoding.UTF8.GetBytes(replyMessage);
+
+            channel.BasicPublish("", ea.BasicProperties.ReplyTo, null, replyBody);
+        };
+
+        channel.BasicConsume(
+            queue: "RRlog", 
+            autoAck: true, 
+            consumer: consumer);
+
+        Console.WriteLine("Microsserviço RRlog");
+        Console.ReadKey();
+    }
+}
